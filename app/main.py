6318232dfd68eb7aafe352224a036e2413faabca
Daniil Gulak <DGulak@alfabank.ru>
2025-01-29 17:04:34 +0300

    some changes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, HTMLResponse
 import sounddevice as sd
 import numpy as np
 import io
 
 app = FastAPI()
 
+# Обработчик для корневого маршрута, отдающий HTML-файл
+@app.get("/", response_class=HTMLResponse)
+async def serve_index():
+    with open("index.html", "r") as f:  # Предполагается, что файл находится в корне проекта
+        return HTMLResponse(content=f.read())
+
 def audio_stream():
     while True:
-        frames, _ = sd.rec(1024, samplerate=48000, channels=1, dtype='int16', blocking=True)
+        frames = sd.rec(1024, samplerate=48000, channels=1, dtype='int16', blocking=True)
         wav_buffer = io.BytesIO()
         wav_buffer.write(frames.tobytes())
         wav_buffer.seek(0)
